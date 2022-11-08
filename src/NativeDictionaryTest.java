fd4b08011039be440cc93e80cdeaa9aef1f7ad61
Otryaskin Andrey <kinlydog@gmail.com>
2022-11-08 23:06:54 +1000

    Remake tests

--- src/NativeDictionaryTest.java
+++ src/NativeDictionaryTest.java
@@ -7,6 +7,8 @@ class NativeDictionaryTest {
 
     @org.junit.jupiter.api.Test
     void isKey() {
+        assertFalse(nd.isKey(""));
+
         for (int i = 0; i < 11; i++) {
             if (i < 5) {
                 nd.put(String.valueOf(i) + 5, String.valueOf(i) + "val");
@@ -22,6 +24,9 @@ class NativeDictionaryTest {
                 assertTrue(nd.isKey(String.valueOf(i)));
             }
         }
+
+        nd.put("05", "test_value");
+        assertTrue(nd.isKey("05"));
     }
 
     @org.junit.jupiter.api.Test
@@ -29,8 +34,8 @@ class NativeDictionaryTest {
         nd.put("123", "123value");
         nd.put("123", "123valuesTwo");
 
-        nd.isKey("123");
-        nd.isKey("321");
+        assertTrue(nd.isKey("123"));
+        assertFalse(nd.isKey("321"));
     }
 
     @org.junit.jupiter.api.Test
@@ -40,5 +45,9 @@ class NativeDictionaryTest {
 
         Object o1 = nd.get("123");
         Object o2 = nd.get("321");
+
+        assertFalse(o1.equals("123values"));
+        assertTrue(o1.equals("123valuesTwo"));
+        assertTrue(o2 == null);
     }
 }
